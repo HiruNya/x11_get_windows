4c94e5a8e1a04d5e656bf91c2997aaeef8cc8b36
Hiruna Jayamanne <hirunav2@gmail.com>
2018-10-14 01:53:53 +1300

    Added an empty line

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,7 @@
 //! If you are going to be using either a [Session] or a [Display] struct more than once,
 //! please use the same one for each time as [x11::xlib::XOpenDisplay] is used when opening,
 //! and [x11::xlib::XCloseDisplay] is used on drop.
+
 #![warn(missing_docs)]
 #![feature(dbg_macro)]
 
